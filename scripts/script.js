4000782792c30fcdfda10bdf05d4ea7a46c3fe12
pavankandula <pavansrivenkatasai@gmail.com>
2019-11-05 12:49:14 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+/* Demo Scripts for Bootstrap Carousel and Animate.css article
+* on SitePoint by Maria Antonietta Perna
+*/
+(function($) {
+    //Function to animate slider captions
+    function doAnimations(elems) {
+      //Cache the animationend event in a variable
+      var animEndEv = "webkitAnimationEnd animationend";
+  
+      elems.each(function() {
+        var $this = $(this),
+          $animationType = $this.data("animation");
+        $this.addClass($animationType).one(animEndEv, function() {
+          $this.removeClass($animationType);
+        });
+      });
+    }
+  
+    //Variables on page load
+    var $myCarousel = $("#carouselExampleIndicators"),
+      $firstAnimatingElems = $myCarousel
+        .find(".carousel-item:first")
+        .find("[data-animation ^= 'animated']");
+  
+    //Initialize carousel
+    $myCarousel.carousel();
+  
+    //Animate captions in first slide on page load
+    doAnimations($firstAnimatingElems);
+  
+    //Other slides to be animated on carousel slide event
+    $myCarousel.on("slide.bs.carousel", function(e) {
+      var $animatingElems = $(e.relatedTarget).find(
+        "[data-animation ^= 'animated']"
+      );
+      doAnimations($animatingElems);
+    });
+
+    /*products carousel*/
+    var carousel = $(".carousel-products"),
+    currdeg  = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e){
+  if(e.data.d=="n"){
+    currdeg = currdeg - 60;
+  }
+  if(e.data.d=="p"){
+    currdeg = currdeg + 60;
+  }
+  carousel.css({
+    "-webkit-transform": "rotateY("+currdeg+"deg)",
+    "-moz-transform": "rotateY("+currdeg+"deg)",
+    "-o-transform": "rotateY("+currdeg+"deg)",
+    "transform": "rotateY("+currdeg+"deg)"
+  });
+}
+
+$(window).scroll(function(){
+  var scrollPos = $(document).scrollTop();
+  if(scrollPos>60){
+    $(".navbar").addClass('bg-dark');
+  }else{
+    $(".navbar").removeClass('bg-dark');
+  }
+  
+});
+
+  })(jQuery);
+
+  
